e3af1012fb0dfc576e726d4cfb04aa690ad6b9c9
Matthias Goetzke <m.goetzke@curasystems.de>
2025-07-16 09:29:05 +0200

    Initial Commit

---
+++ MicSwitcher/MicSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicSwitcherApp.swift
+//  MicSwitcher
+//
+//  Created by Matthias Goetzke on 16.07.25.
+//
+
+import SwiftUI
+
+@main
+struct MicSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MicSwitcherTests/MicSwitcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  MicSwitcherTests.swift
+//  MicSwitcherTests
+//
+//  Created by Matthias Goetzke on 16.07.25.
+//
+
+import Testing
+@testable import MicSwitcher
+
+struct MicSwitcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
